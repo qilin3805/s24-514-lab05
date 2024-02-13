80fa6d261bf3006c0873cedbd87a21364f891821
qilin3805 <147442831+qilin3805@users.noreply.github.com>
2024-02-13 14:39:40 -0500

    getTotalAdded with inheritance.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,47 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntLinkedList{
+    private int count = 0;
 
+    /**
+     * Adds the specified int to the list.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        count++;
+        return super.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     * Calls add() on each element of list. Can be overwritten
+     * for more specific behavior.
+     *
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    public boolean addAll(IntegerList list) {
+
+        boolean success = false;
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            success |= add(list.get(i));
+        }
+
+        return success;
+    }
+
+    /**
+     * Count how many elements have been added.
+     * 
+     * @return number of attempted insertions
+     */
+    public int getTotalAdded() {
+        return count;
+    }
 }
